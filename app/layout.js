d98a81e614f4f48b59ca0894cf38a44a48f61664
rogergrubb <roger@grubb.net>
2026-04-21 07:30:07 -0700

    feat: add layout

---
+++ app/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "N1S Analytics",
+  description: "Central analytics for Number One Son content network",
+};
+export default function RootLayout({ children }) {
+  return <html lang="en"><body>{children}</body></html>;
+}
+
